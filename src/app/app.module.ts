ccdd76df07f022ffd8099f5eb6140317a534c277
giovfranz1234 <giovfranz@gmail.com>
2022-07-31 20:10:21 -0400

    En camino spring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8082/',
+        realm: 'HelpDesk',
+        clientId: 'angular-HelpDesk'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow:"standard",
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      loadUserProfileAtStartUp:true
+    });
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +30,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+    multi: true,
+    deps: [KeycloakService]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
